efc121bdeae7578a415a5d91f43cedfba17aa435
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-02-23 10:57:37 -0300

    1234567: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,12 @@
+# Solution
+
+mkdir praticas_desenvolvimento_software
+ln -s ./praticas_desenvolvimento_software devsoft
+
+mkdir praticas_desenvolvimento_software/abigail
+ln -s abigail ./praticas_desenvolvimento_software/abby
+ln -s abigail ./praticas_desenvolvimento_software/1234567
+
+mkdir praticas_desenvolvimento_software/abigail/lecture1
+
+touch praticas_desenvolvimento_software/abigail/lecture1/empty.txt
