befec1fe2cf0d054ba35168285b3c4b276c3777b
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-02-23 23:24:44 -0300

    8042606: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,6 @@
+mkdir -p praticas_desenvolvimento_software/SauloFuruta/lecture1
+ln -s praticas_desenvolvimento_software devsoft
+cd praticas_desenvolvimento_software
+ln -s SauloFuruta Paulo
+ln -s SauloFuruta 8042606
+touch SauloFuruta/lecture1/empty.txt
