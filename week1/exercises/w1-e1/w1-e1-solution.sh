c8b11c1de8aa27ee96972f5cebae80dc17a5cd37
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-02-24 08:57:04 -0300

    8587100: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,16 @@
+mkdir praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software devsoft
+
+cd ./praticas_desenvolvimento_software
+
+mkdir Manuel
+ln -s Manuel Puyol
+ln -s Manuel 8587100
+
+cd ./Manuel
+
+mkdir lecture1
+
+cd ./lecture1
+
+touch empty.txt
