7e8c1c89fd43e42dda2da8834d47369433d7e4c3
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-02-25 15:55:39 -0300

    8042818: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,11 @@
+cd /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1
+mkdir praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software devsoft
+cd ./praticas_desenvolvimento_software
+mkdir Dante
+ln -s Dante Dan7e
+ln -s Dante 8042818
+cd ./Dante
+mkdir lecture1
+cd ./lecture1
+touch empty.txt
