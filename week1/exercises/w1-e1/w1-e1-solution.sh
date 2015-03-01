0a3a0f6bc33aa09c1fcd8fc205ad18a1cedd9cb1
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-01 08:34:13 -0300

    8583771: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,10 @@
+mkdir praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software ./devsoft
+cd devsoft
+mkdir "PedroGryzinsky"
+ln -s "PedroGryzinsky" ./"Pedro"
+ln -s "PedroGryzinsky" ./"8583771"
+cd "PedroGryzinsky"
+mkdir lecture1
+cd lecture1
+touch empty.txt
