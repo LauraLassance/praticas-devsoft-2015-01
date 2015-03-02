0141ac62c25d726fae50944f88ae058437c273dc
Débora Setton <debora.setton@infosimples.com.br>
2015-03-02 19:15:01 -0300

    Update W2-E1 description.

--- week2/exercises/w2-e1/w2-e1-spec.rb
+++ week2/exercises/w2-e1/w2-e1-spec.rb
@@ -23,14 +23,13 @@ describe 'Exercise W2-E1' do
 
   specify "Branch u_#{USER_NUSP}/b1 exists" do
     output = exec_cmd('git branch')
-    expect(output).to match(/^\s+u_#{USER_NUSP}\/b1\n/)
+    expect(output).to match(/^\*?\s+u_#{USER_NUSP}\/b1/)
   end
 
-  specify "Branch u_#{USER_NUSP}/w2-e1 points to master" do
-
+  specify "Branch u_#{USER_NUSP}/b1 points to master" do
     output = exec_cmd('git log --all --oneline --decorate | grep master')
     expect(output).to match(/master/)
-    expect(output).to match(/u_#{USER_NUSP}\/w2-e1/)
+    expect(output).to match(/u_#{USER_NUSP}\/b1/)
   end
 
   specify "User is currently on branch u_#{USER_NUSP}/b1" do
