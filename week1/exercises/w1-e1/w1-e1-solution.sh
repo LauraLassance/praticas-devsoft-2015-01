6086ca2e60f83377ebee2bb470560f3bee642cd8
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-02 01:55:14 -0300

    7278052: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software devsoft
+mkdir praticas_desenvolvimento_software/Joao
+cd praticas_desenvolvimento_software
+ln -s Joao JB
+ln -s Joao 7278052
+cd ..
+mkdir praticas_desenvolvimento_software/Joao/lecture1
+touch praticas_desenvolvimento_software/Joao/lecture1/empty.txt
