f099996003427fc289a535c7c52916e1c2dedcbe
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-02 16:22:58 -0300

    7700827: W1-E2.

--- week1/exercises/w1-e2/w1-e2-solution.sh
+++ week1/exercises/w1-e2/w1-e2-solution.sh
@@ -2,9 +2,6 @@
 
 wget http://www.tldp.org/LDP/intro-linux/html/intro-linux.html
 
-x=`cat intro-linux.html | grep -o Linux | wc -w`
-y=`cat intro-linux.html | grep -o linux | wc -w`
-z=`cat intro-linux.html | grep -io linux | wc -w`
-echo "Linux: $x"
-echo "linux: $y"
-echo "linux (case-insensitive): $z"
+echo "Linux: $(cat intro-linux.html | grep -o Linux | wc -w)"
+echo "linux: $(cat intro-linux.html | grep -o linux | wc -w)"
+echo "linux (case-insensitive): $(cat intro-linux.html | grep -io linux | wc -w)"
