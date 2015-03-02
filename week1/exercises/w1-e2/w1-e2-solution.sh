2644ebe68be2f4ccbddd5035602d478a0cd58d98
Anonymous (Práticas Devsoft) <treinamento@infosimples.com.br>
2015-03-02 17:36:20 -0300

    8588199: W1-E2.

---
+++ week1/exercises/w1-e2/w1-e2-solution.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+wget -O saida.txt http://www.tldp.org/LDP/intro-linux/html/intro-linux.html
+echo "Linux: "
+grep -o 'Linux' saida.txt | wc -l
+echo "linux: "
+grep -o 'linux' saida.txt | wc -l
+echo "linux (case-insensitive): "
+grep -io 'linux' saida.txt | wc -l
