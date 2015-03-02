24c0ddb71de68261d08d6dfffa7c8595e5134197
Anonymous (Práticas Devsoft) <treinamento@infosimples.com.br>
2015-03-02 18:15:27 -0300

    8587729: W1-E4.

--- week1/exercises/w1-e4/w1-e4-solution.sh
+++ week1/exercises/w1-e4/w1-e4-solution.sh
@@ -1,1 +1,20 @@
-echo "hey"
+#!/bin/bash
+
+if [ "$1" == "cd" ]; then
+	echo "Vai para determinado diretório, ex cd Dev vai para a pasta Dev"
+elif [ "$1" == "ls" ]; then
+	echo "Mostra as pastas e arquivos do diretório atual"
+elif [ "$1" == "pwd" ]; then
+	echo "mostra o caminho da home até o diretório atual"
+elif [ "$1" == "cat" ]; then
+	echo "concatena de um arquivo para outro ou para alguma saída"
+elif [ "$1" == "rm" ]; then
+	echo "remove um arquivo ou diretório"
+elif [ "$1" == "chmod" ]; then
+	echo "altera as permissões de um arquivo, ex chmod 777 = todas as permissões"
+elif [ "$1" == "man" ]; then
+	echo "manual, descreve um comando e suas aplicações"
+elif [ "$1" == "time" ]; then
+	echo "mostra o tempo de execução de um comando"
+fi	
+
