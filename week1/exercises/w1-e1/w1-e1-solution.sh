ae5f8b3fccf8a34063d0cdf43cc2594e899e495c
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-02 18:25:58 -0300

    8040991: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,6 @@
+mkdir -p praticas_desenvolvimento_software/PedroStrabeli/lecture1
+ln -s praticas_desenvolvimento_software devsoft
+cd praticas_desenvolvimento_software
+ln -s PedroStrabeli Pedrinho
+ln -s PedroStrabeli 8040991
+touch PedroStrabeli/lecture1/empty.txt
