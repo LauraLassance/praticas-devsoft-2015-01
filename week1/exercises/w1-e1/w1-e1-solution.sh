67b9b6b82243e77555d1f8fdfe9b6603cb03b512
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-02 16:32:19 -0300

    8586944: W1-E1.

--- week1/exercises/w1-e1/w1-e1-solution.sh
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -1,10 +1,10 @@
 mkdir praticas_desenvolvimento_software
 ln -s praticas_desenvolvimento_software devsoft
 cd praticas_desenvolvimento_software
-mkdir Mateus
-ln -s Mateus Saito
-ln -s Mateus 8587858
-cd Mateus
+mkdir Erick
+ln -s Erick Noob
+ln -s Erick 8586944
+cd Erick
 mkdir lecture1
 cd lecture1
 touch empty.txt
