26dad8bff68b099bf5e389693b7ba2271b78693d
Anonymous (Práticas Devsoft) <treinamento@infosimples.com.br>
2015-03-01 23:14:08 -0300

    8588348: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,11 @@
+mkdir praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software devsoft
+cd praticas_desenvolvimento_software
+mkdir Renan_Hidemi_Kataoka
+ln -s Renan_Hidemi_Kataoka renan.kataoka
+ln -s Renan_Hidemi_Kataoka 8588348
+cd Renan_Hidemi_Kataoka
+mkdir lecture1
+cd lecture1
+touch empty.txt
+
