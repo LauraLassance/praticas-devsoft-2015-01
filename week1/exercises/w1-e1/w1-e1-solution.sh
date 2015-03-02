cd32f940f7ceb66ec1b1d6b7811de2ca32f770c0
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-02 00:04:59 -0300

    8038581: W1-E1.

---
+++ week1/exercises/w1-e1/w1-e1-solution.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+chmod +x /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/w1-e1-solution.sh
+chmod 777 /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/w1-e1-solution.sh
+cd /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1
+mkdir praticas_desenvolvimento_software
+ln -s /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/devsoft  
+cd /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software
+mkdir Marina
+ln -s /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software/Marina /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software/Ma
+ln -s /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software/Marina /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software/8038581
+cd /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software/Marina
+mkdir lecture1
+cd /home/ubuntu/Dev/praticas-devsoft/infosimples-repo/week1/exercises/w1-e1/praticas_desenvolvimento_software/Marina/lecture1
+cat > empty.txt
+exit 0
