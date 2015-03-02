ae492f7ac0018f5449aaee176829fc7c760c9fe1
Anonymous (Práticas Devsoft) <treinamento@infosimples.com.br>
2015-03-02 16:24:10 -0300

    8040441: W1-E4.

--- week1/exercises/w1-e4/w1-e4-solution.sh
+++ week1/exercises/w1-e4/w1-e4-solution.sh
@@ -1,1 +1,23 @@
-echo "hey"
+COMMAND=$1
+
+case $COMMAND in
+	man)
+		echo "Abre o manual de algum comando desejado.";;
+	cd)
+		echo "Acessa o diretorio desejado.";;
+	ls)
+		echo "Exibe o conteudo do diretorio em que o usuario esta.";;
+	pwd)
+		echo "Exibe o caminho completo do diretorio em que o usuario esta.";;
+	cat)
+		echo "Exibe o conteudo do ficheiro desejado.";;
+	chmod)
+		echo "Muda as configuracoes de protecao de um ficheiro ou diretorio.";;
+	rm)
+		echo "Remove o ficheiro desejado.";;
+	mv)
+		echo "Move o ficheiro ou diretorio desejado.";;
+	time)
+		echo "Mede o tempo de execucao de um determinado programa.";;
+esac 	  
+
