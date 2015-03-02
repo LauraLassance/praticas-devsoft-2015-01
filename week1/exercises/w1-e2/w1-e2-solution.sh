d22d374255b8c156abc2da924f175d21c184d125
Anonymous - Práticas Devsoft <treinamento@infosimples.com.br>
2015-03-02 02:30:27 -0300

    7278052: W1-E2.

---
+++ week1/exercises/w1-e2/w1-e2-solution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -q http://www.tldp.org/LDP/intro-linux/html/intro-linux.html
+echo "Linux: `grep -o Linux intro-linux.html | wc -l`"
+echo "linux: `grep -o linux intro-linux.html | wc -l`"
+echo "linux (case-insensitive): `grep -io linux intro-linux.html | wc -l`"
